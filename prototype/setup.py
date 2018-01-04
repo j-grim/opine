fc8931e8f05b891945a2aed475556e33e8b72b75
j-grimwood <jessicamgrimwood@gmail.com>
2018-01-03 21:31:21 -0800

    Updated setup.py

---
+++ prototype/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Opineprototype',
+    version='0.01',
+    description='The beginning of great things',
+    author='Jess Grimwood',
+    author_email='jessicamgrimwood@gmail.com',
+    )
