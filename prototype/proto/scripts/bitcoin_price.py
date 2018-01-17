6e4275cd60d83005e177b841f45afd261e59f397
j-grimwood <jessicacolding@gmail.com>
2018-01-17 13:40:37 -0800

    Fixed lack of header in json request

--- prototype/proto/scripts/bitcoin_price.py
+++ prototype/proto/scripts/bitcoin_price.py
@@ -3,16 +3,11 @@
 import json
 import urllib.request
 
-exchange_url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-user_agent= 'Mozilla/5.0 (Windows NT 6.1; Win64; X64)'
-values = {'name' : 'Mischa Kolding',
-        'location' : 'Los Angeles',
-        'language' : 'Python' }
-headers = {'User-Agent' : user_agent }
 
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(exchange_url, data, headers)
-with urllib.request.urlopen(req) as response:
-    the_json = response.read()
-print(the_json)
+url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+hdr = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
+req = urllib.request.Request(url, headers=hdr)
+
+readdata = urllib.request.urlopen(req)
+json_data = readdata.read()
+print(json_data)
