01674cf453e2c26a1525e91f2f7275b179dd6248
j-grimwood <jessicamgrimwood@gmail.com>
2018-01-23 13:36:27 -0800

    Added API docu notes. Need to refactor with OOP solution

--- prototype/proto/scripts/bitcoin_price.py
+++ prototype/proto/scripts/bitcoin_price.py
@@ -1,9 +1,12 @@
 #This is the API for BTC price request. 
+# Make this OBJ oriented. 
+# Average all the amounts, and push that to the program
 
 import json
 import urllib.request
 
 #Coindesk#
+
 
 url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 hdr = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
@@ -20,3 +23,25 @@ current_us_price = json_dict['bpi']['USD']['rate_float']
 
 print(current_utc_time)
 print(current_us_price)
+
+
+#Coinbase API
+
+
+url = "https://api.coinbase.com/v2/prices/spot?currency=USD"
+#https://developers.coinbase.com/docs/wallet/guides/price-data  <- API Doc
+#has a pip installable lib. Dependency undesirable, but better than cURL.
+
+
+
+#Bitstamp API
+#https://www.bitstamp.net/api/
+
+
+
+#Coinmarketcap API
+# https://coinmarketcap.com/api/
+# Will offer historical data
+
+#Cryptocompare API (Very robust, in terms of data)
+#https://www.cryptocompare.com/api/#-api-data-pricehistorical-
