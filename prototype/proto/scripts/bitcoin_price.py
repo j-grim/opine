3aafe5c1c9e19b17ce506a930b86059004aab4ae
j-grimwood <jessicamgrimwood@gmail.com>
2018-01-23 13:25:08 -0800

    parsed/variablized json

--- prototype/proto/scripts/bitcoin_price.py
+++ prototype/proto/scripts/bitcoin_price.py
@@ -3,6 +3,7 @@
 import json
 import urllib.request
 
+#Coindesk#
 
 url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 hdr = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
@@ -10,4 +11,12 @@ req = urllib.request.Request(url, headers=hdr)
 
 readdata = urllib.request.urlopen(req)
 json_data = readdata.read()
-print(json_data)
+#print(json_data)
+
+json_dict = json.loads(json_data)
+
+current_utc_time = json_dict['time']['updated']
+current_us_price = json_dict['bpi']['USD']['rate_float']
+
+print(current_utc_time)
+print(current_us_price)
