46804b5837c68d057740a74e1f34131d359b3c83
j-grimwood <jessicamgrimwood@gmail.com>
2018-01-25 16:29:34 -0800

    Building in OOP structure.

--- prototype/proto/scripts/bitcoin_price.py
+++ prototype/proto/scripts/bitcoin_price.py
@@ -6,7 +6,7 @@ import json
 import urllib.request
 
 #Coindesk#
-
+'''
 
 url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 hdr = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
@@ -45,3 +45,27 @@ url = "https://api.coinbase.com/v2/prices/spot?currency=USD"
 
 #Cryptocompare API (Very robust, in terms of data)
 #https://www.cryptocompare.com/api/#-api-data-pricehistorical-
+'''
+
+import json
+import urllib.request
+
+class BTC_API:
+
+    def __init__(self, url, api_id):
+        self.url = url
+        self.api_id = api_id
+
+    def build_api_url(self):
+        #build api url or pass it to args
+
+    def api_req(self):
+        #GET request
+
+    def return_data(self):
+        # Parse JSON, return data
+
+
+api_url_dict = { 'Coindesk' : 'https://api.coindesk.com/v1/bpi/currentprice.json' , 'Coinbase' : 'https://api.coinbase.com/v2/prices/spot?currency=USD', 'Bitstamp' : ' ', 'CoinMarketCap' : ' ' , 'CryptoCompare' : ' ' }  
+
+# Loop through dict to pull both name and url into variables, instantiate classes, pull data, parse, and boom!
