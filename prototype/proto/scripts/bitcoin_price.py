07f179d376b322b40e0214368947dec740770bfa
j-grimwood <jessicamgrimwood@gmail.com>
2018-01-26 16:12:05 -0800

    Barebones OOP API call implemented

--- prototype/proto/scripts/bitcoin_price.py
+++ prototype/proto/scripts/bitcoin_price.py
@@ -4,9 +4,9 @@
 
 import json
 import urllib.request
-
-#Coindesk#
 '''
+#Coindesk#
+
 
 url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
 hdr = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
@@ -27,17 +27,12 @@ print(current_us_price)
 
 #Coinbase API
 
-
 url = "https://api.coinbase.com/v2/prices/spot?currency=USD"
 #https://developers.coinbase.com/docs/wallet/guides/price-data  <- API Doc
 #has a pip installable lib. Dependency undesirable, but better than cURL.
 
-
-
 #Bitstamp API
 #https://www.bitstamp.net/api/
-
-
 
 #Coinmarketcap API
 # https://coinmarketcap.com/api/
@@ -47,25 +42,51 @@ url = "https://api.coinbase.com/v2/prices/spot?currency=USD"
 #https://www.cryptocompare.com/api/#-api-data-pricehistorical-
 '''
 
-import json
-import urllib.request
-
 class BTC_API:
 
     def __init__(self, url, api_id):
         self.url = url
         self.api_id = api_id
 
-    def build_api_url(self):
-        #build api url or pass it to args
+    def btc_api_call(self):
 
-    def api_req(self):
-        #GET request
+        hdr = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
+        req = urllib.request.Request(url, headers=hdr)
+        readdata = urllib.request.urlopen(req)
+        json_data = readdata.read()
 
-    def return_data(self):
-        # Parse JSON, return data
+        api_req(self, json_data)
+        json_dict = json.loads(json_data)
+
+        # Refactor this hideousness...
+        if api_id = Coindesk:
+
+            current_utc_time = json_dict['time']['updated']
+            current_us_price = json_dict['bpi']['USD']['rate_float']
+            
+            return(current_utc_time)
+            return(current_us_price)
+
+        elif api_id = 'Coinbase':
+
+        elif api_id = 'Bitstamp':
+
+        elif api_id = 'CoinMarketCap':
+
+        elif api_id = 'CryptoCompare':
+
+        else:
+            print('API Retrieval Failed')
 
 
-api_url_dict = { 'Coindesk' : 'https://api.coindesk.com/v1/bpi/currentprice.json' , 'Coinbase' : 'https://api.coinbase.com/v2/prices/spot?currency=USD', 'Bitstamp' : ' ', 'CoinMarketCap' : ' ' , 'CryptoCompare' : ' ' }  
+api_url_dict = { 
+
+        'Coindesk' : 'https://api.coindesk.com/v1/bpi/currentprice.json' , 
+        'Coinbase' : 'https://api.coinbase.com/v2/prices/spot?currency=USD', 
+        'Bitstamp' : ' ', 
+        'CoinMarketCap' : ' ' , 
+        'CryptoCompare' : ' ' 
+        
+        }  
 
 # Loop through dict to pull both name and url into variables, instantiate classes, pull data, parse, and boom!
