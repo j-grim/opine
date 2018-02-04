c69600037fb3b220e7680ec5aa65e6c037a42e95
j-grimwood <jessicamgrimwood@gmail.com>
2018-02-03 22:52:54 -0800

    Drastically modified code. Now using jsonpath. Will implement .ini style config for API call config.

--- prototype/proto/scripts/bitcoin_price.py
+++ prototype/proto/scripts/bitcoin_price.py
@@ -5,112 +5,54 @@
 
 import json
 import urllib.request
-
+from jsonpath_rw import parse as parse_jsonpath
 
 class BtcAPI:
 
-    def __init__(self, url, api_id, json_tree):
+    def __init__(self, url, api_id, json_key):
         self.url = url
         self.api_id = api_id
-        self.json_tree = json.tree
+        self.json_key = json_key
 
     def btc_api_call(self):
 
         hdr = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
-        req = urllib.request.Request(url, headers=hdr)
+        req = urllib.request.Request(self.url, headers=hdr)
         readdata = urllib.request.urlopen(req)
         json_data = readdata.read()
 
         json_dict = json.loads(json_data)
-        print(json_dict[json_tree]) #syntax here might be bad
-
-'''
-url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-api_id ="Coindesk"
-
-get_price = BtcAPI(url, api_id)
-get_price.btc_api_call()
-'''
+        results = parse_jsonpath(self.json_key).find(json_dict)
+        print(results)
 
 
 class Price:
     
-    def __init__(self, api_id, url, json_tree)
+
+    def __init__(self, api_id, url, json_key):
         
         self.api_id = api_id
         self.url = url
-        self.json_tree = json_tree
+        self.json_key = json_key
     
     def pass_for_request(self):
 
-        get_price = BtcAPI(url, api_id, json_tree)
+        get_price = BtcAPI(self.url, self.api_id, self.json_key)
         get_price.btc_api_call()
-
+        
+        
 def Coindesk():
-    coindesk = Price(api_id ="coindesk", url = "https://api.coindesk.com/v1/bpi/currentprice.json", json_tree = "['time']['updated']")
+    coindesk = Price("coindesk","https://api.coindesk.com/v1/bpi/currentprice.json","time.updated")
     coindesk.pass_for_request()
 
-'''
 def Bitstamp():
-    bitstamp = Price(api_id="bitstamp", url = "https://www.bitstamp.net/api/ticker/", json_tree ="['last']")
+    bitstamp = Price("bitstamp","https://www.bitstamp.net/api/ticker/", "last")
 
 
 
-def CoinMarketCap:
+#def CoinMarketCap:
 
-def CryptoCompare:
-'''
-
+#def CryptoCompare:
 
 
-'''        
-
-class Coindesk:
-    
-
-    api_id = 'Coindesk'
-    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'               
-    json_tree = json_dict['time']['updated'] # Reference this after json data is pulled, to variablize the output and store it.
-
-    def __init__(self):
-    
-        self.current_us_price = current_us_price
-    
-coindesk_url = Coindesk()
-coindeskoutput = coindesk_url.url
-print(coindeskoutput)
-
-
-
-class Bitstamp:
-
-    json_tree = json_dict['last']
-
-
-    def __init__(self, json_dict):
-
-#        self.current_us_price = current_us_price
-
-
-class CoinMarketCap:
-    
-    api_id = 'CoinMarketCap'
-    url = ' '
-    json_tree = ' ' #add this when you know what the tree looks like
-
-    def __init__(self):
-
-        self.current_us_price = current_us_price
-
-
-class CryptoCompare:
-
-    api_id = 'CryptoCompare'
-    url = ' '
-    json_tree = ' ' #add this when you know what the tree looks like
-
-    def __init__(self):
-
-        self.current_us_price = current_us_price
-
-'''
+Coindesk()
